f06827c5865ec45ad7482384dfde2051d4a5af6d
Brent's Retina Mac <bweber@redsparklabs.com>
2013-06-06 10:59:51 -0600

    home controller tests
    Signed-off-by: Brent's Retina Mac <bweber@redsparklabs.com>

--- test/functional/home_controller_test.rb
+++ test/functional/home_controller_test.rb
@@ -6,7 +6,14 @@ class HomeControllerTest < ActionController::TestCase
     assert_response :success
   end
 
-  test "should get dashboard" do
+  test "dashboard should be unavailable to users not logged in" do
+    get :dashboard
+    assert_redirected_to new_user_session_path
+    #assert_response :success
+  end
+
+  test "dashboard should be available to logged in users" do 
+    sign_in users(:one)
     get :dashboard
     assert_response :success
   end
